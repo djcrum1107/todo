1846e476ab596becfba9e13b01f0007d136b07fe
djcrum1107 <djcrum1107@gmail.com>
2021-10-19 00:17:44 -0400

    Set up basic task creation and reading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import {addTask, readTasks} from './taskController'
 import './style.css';
+
+function collectNavigation(){
+    
+}
+
+function initWebsite(){
+    const mainBody = document.body;
+    addTask('Test');
+    addTask('Test');
+    addTask('Test');
+    addTask('Test');
+    console.log(readTasks());
+}
+
+initWebsite();
---
+++ src/taskController.js
@@ -0,0 +1,62 @@
+import {getDate} from 'date-fns';
+
+let taskList = []
+let taskIDCount = 0;
+
+const task = (id, title, dueDate = new Date().toLocaleDateString(), repeat = "none") => {
+    let taskID = id;
+    let details = title;
+    let archived = false;
+
+    const getTaskID = () => {
+        return taskID;
+    }
+
+    const getTitle = () => {
+        return title;
+    }
+
+    const setTitle = (newTitle) => {
+        title = newTitle;
+    }
+
+    const getDetails = () => {
+        return details;
+    }
+
+    const setDetails = (newDetails) => {
+        details = newDetails;
+    }
+
+    const getDueDate = () => {
+        return dueDate;
+    }
+
+    const setDueDate = (newDueDate) => {
+        dueDate = newDueDate;
+    }
+
+    const isArchived = () => {
+        return archived; 
+    }
+
+    const setArchived = (archivedStatus) =>{
+        archived = archivedStatus;
+    }
+
+    return {getTaskID, getTitle, setTitle, getDetails, setDetails, getDueDate, setDueDate, repeat, isArchived, setArchived};
+};
+
+function addTask(title){
+    taskList.push(task(taskIDCount++, title));
+}
+
+function listTask(id){
+    return taskList(id);
+}
+
+function readTasks(){
+    return taskList;
+}
+
+export {addTask, readTasks};
